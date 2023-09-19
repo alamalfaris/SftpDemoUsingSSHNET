2570972f9a9abc22498638c13ea077f621cc5ad9
Alam Alfaris <alamalfaris97@gmail.com>
2023-09-19 15:26:25 +0700

    Add project files.

---
+++ SftpDemoUsingSSHNET/Program.cs
@@ -0,0 +1,53 @@
+﻿using Renci.SshNet;
+
+namespace SftpDemoUsingSSHNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(SftpClient client = new SftpClient(new PasswordConnectionInfo("host/ip", "username", "password")))
+            {
+                client.Connect();
+
+                // Upload file to SFTP server
+                //string sourceFile = $"C:\\mydata\\alam\\ttd.png";
+                //using (Stream stream = File.OpenRead(sourceFile))
+                //{
+                //    client.UploadFile(stream, $"\\TestSftp\\" + Path.GetFileName(sourceFile));
+                //}
+
+                // Download file to SFTP Client
+                //string serverFile = $"\\TestSftp\\test.txt";
+                //string localFile = $"C:\\mydata\\alam\\" + Path.GetFileName(serverFile);
+                //using (Stream stream = File.OpenWrite(localFile))
+                //{
+                //    client.DownloadFile(serverFile, stream);
+                //}
+
+                // Read file
+                //string serverFile = $"\\TestSftp\\test.txt";
+                //var results = client.ReadAllText(serverFile);
+                //Console.WriteLine(results);
+
+                // Delete file
+                //string serverFile = $"\\TestSftp\\ttd.png";
+                //client.DeleteFile(serverFile);
+
+                // List
+                string serverDirectory = $"\\TestSftp";
+                var files = client.ListDirectory(serverDirectory).ToList();
+                if (files.Any())
+                {
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file.Name);
+                        Console.WriteLine(file.FullName);
+                    }
+                }
+
+                client.Disconnect();
+            }
+        }
+    }
+}
